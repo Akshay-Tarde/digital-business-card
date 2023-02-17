54a39644713f1bbf1be70030dd691540534d72a0
Akshay-Tarde <akshaymtarde@gmail.com>
2023-02-17 17:51:23 +0530

    Added the icons and links in the footer. Styled the footer.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import twitterIcon from "./assets/twitter-icon.png"
+import githubIcon from "./assets/github-icon.png"
 
 export default function Footer() {
     return (
-        <footer className="footer"></footer>
+        <footer className="footer">
+            <div className="icons">
+                <a href="https://twitter.com/_4kshay_" target={"_blank"}><img src={twitterIcon} alt="Twitter icon"/></a>
+                <a href="https://github.com/Akshay-Tarde/" target={"_blank"}><img src={githubIcon} alt="GitHub icon"/></a>
+            </div>
+        </footer>
     )
 }
